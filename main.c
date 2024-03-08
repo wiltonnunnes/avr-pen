e9d23d1d29f50825fe76258c83a3136741cdf45b
wiltonnunnes <will_nunnes@hotmail.com>
2024-03-07 23:40:51 -0300

    Commit 07/03/2024 23:40

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <avr/io.h>
 
+void UART_init(uint16_t ubrr) {
+    UBRR0L = (uint8_t)(ubrr & 0xFF);
+    UBRR0H = (uint8_t)(ubrr >> 8);
+
+    UCSR0B |= (1 << RXEN0) | (1 << TXEN0);
+}
+
 int main(void) {
     DDRD |= (1 << PD2);
     DDRD &= ~(1 << PD4);
+
+    UART_init(103);
 
     while (1) {
         if ((PIND & (1 << PD4)) == 0)
